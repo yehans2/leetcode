0c93afe3ad4086517c62d61e478bc52b7e7cc3ce
Edwin Lee <91001059+yehans2@users.noreply.github.com>
2022-05-11 01:10:53 +0800

    Time: 83 ms (9.81%), Space: 14.3 MB (19.32%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -7,23 +7,18 @@ class Solution:
             
             if nums[m] == target:
                 return m
-            elif nums[m] > target:
-                if nums[l] <= nums[m]:
-                    if nums[l] > target:
-                        l = m + 1
-                    else:
-                        r = m-1
-                else:
+            elif nums[m] >= nums[l]:
+                if nums[l] <= target <=nums[m]:
                     r = m - 1
+                else:
+                    l = m + 1
             else:
-                if nums[l] <= nums[m]:
+                if nums[m] <= target <= nums[r]:
                     l = m + 1
                 else:
-                    if target <= nums[r]:
-                        l = m + 1
-                    else:
-                        r = m - 1
+                    r = m - 1
+
         return -1 
                     
                     
-                
+  
