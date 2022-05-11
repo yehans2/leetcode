0315b2a6016bb7a131084552b0216ac7848c5e3c
Edwin Lee <91001059+yehans2@users.noreply.github.com>
2022-05-11 20:02:23 +0800

    Time: 57 ms (48.31%), Space: 14.3 MB (19.27%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        l, r, = 0, len(nums)-1
+        l, r = 0, len(nums)-1
         
         while l <= r:
             m = (l+r) // 2
@@ -8,7 +8,7 @@ class Solution:
             if nums[m] == target:
                 return m
             elif nums[m] >= nums[l]:
-                if nums[l] <= target <=nums[m]:
+                if nums[l] <= target <= nums[m]:
                     r = m - 1
                 else:
                     l = m + 1
@@ -17,8 +17,7 @@ class Solution:
                     l = m + 1
                 else:
                     r = m - 1
-
-        return -1 
-                    
+        return -1
+     
                     
   
