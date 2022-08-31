b38138c6ce6eed3aa624bb3301e42ef3b8edba23
Edwin Lee <91001059+yehans2@users.noreply.github.com>
2022-08-31 20:57:25 +0800

    Time: 170 ms (20.38%), Space: 14.3 MB (47.25%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.py
@@ -1,23 +1,26 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        hashmap = {}
-        
-        row = len(mat)
-        col = len(mat[0])
-        
-        for i in range(row):
-            for j in range(col):
-                if i - j not in hashmap:
-                    hashmap[i-j] = [mat[i][j]]
-                else:
-                    hashmap[i-j].append(mat[i][j])
-        
-        for key in hashmap:
-            hashmap[key].sort(reverse=True)
-            
-        for i in range(row):
-            for j in range(col):
-                mat[i][j] = hashmap[i-j][-1]
-                hashmap[i-j].pop()
-        
-        return mat
+        row, col = len(mat), len(mat[0])
+        total_num = row * col
+
+        for i in range(1, row + col - 2):
+            if i < row:
+                start_row, start_col = row - i - 1, 0
+            else:
+                start_row, start_col = 0, i - row + 1
+
+            diag = []
+            while start_row < row and start_col < col:
+                diag.append(mat[start_row][start_col])
+                start_row += 1
+                start_col += 1
+
+            diag.sort()
+            start_row -= 1
+            start_col -= 1
+            while start_row >= 0 and start_col >= 0:
+                mat[start_row][start_col] = diag.pop()
+                start_row -= 1
+                start_col -= 1
+
+        return(mat)
