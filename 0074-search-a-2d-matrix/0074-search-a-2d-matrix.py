aeac80deeb1db71c7ccd0816fbf0a6fe72a55b61
Edwin Lee <91001059+yehans2@users.noreply.github.com>
2022-11-30 18:18:38 +0800

    Time: 49 ms (88.44%), Space: 14.5 MB (8.78%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -1,10 +1,14 @@
 class Solution:
-    def searchMatrix(self, matrix, target):
-        i = bisect_left(matrix, [target])
-        print(i)
-        print(len(matrix))
-        if i < len(matrix) and matrix[i][0] == target:
-            return True
-        row = matrix[i-1]
-        j = bisect.bisect_left(row, target)
-        return j < len(row) and row[j] == target
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        r = bisect_left(matrix, target, key=lambda row: row[-1]) 
+        print(r)
+        return r < len(matrix) and matrix[r][bisect_left(matrix[r], target)] == target
+# An alternative approach is to binary search across matrix in its entirety, as you would a 1D array.
+
+# class Solution:
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         n = len(matrix[0])
+#         def get(idx: int) -> int:
+#             r, c = divmod(idx, n)
+#             return matrix[r][c]
+#         return get(bisect_left(range(len(matrix)*n-1), target, key=get)) == target
